ca4df11510c67ce808d544a7647cb138ba2a276e
海大富 <haizi.zh@gmail.com>
2015-01-08 17:55:08 +0800

    MOD: 调整架构

--- processors/__init__.py
+++ processors/__init__.py
@@ -1,3 +1,7 @@
+from Queue import Empty
+import traceback
+import signal
+
 __author__ = 'zephyre'
 
 import gevent
@@ -10,7 +14,10 @@ class BaseProcessor(object):
         self.progress = 0
         self.total = 0
         self.concur = BaseProcessor.args_builder().concur
-        self.tasks = Queue()
+        self.maxsize = 1000
+        self.op_done = False
+        self.tasks = Queue(self.maxsize)
+        self.jobs = []
 
     @staticmethod
     def args_builder():
@@ -21,22 +28,36 @@ class BaseProcessor(object):
         args, leftovers = parser.parse_known_args()
         return args
 
+    def join(self):
+        self.op_done = True
+        gevent.joinall(self.jobs[1:])
+        gevent.kill(self.jobs[0])
+
     def run(self):
         def worker():
-            while not self.tasks.empty():
-                task = self.tasks.get()
-                task()
+            while not self.tasks.empty() or not self.op_done:
+                try:
+                    task = self.tasks.get(timeout=1)
+                except Empty:
+                    continue
+
+                try:
+                    task()
+                except Exception:
+                    traceback.print_exc()
+
                 gevent.sleep(0)
 
         def timer():
-            while not self.tasks.empty():
+            while not self.tasks.empty() or not self.op_done:
                 print 'Progress: %d / %d' % (self.progress, self.total)
                 gevent.sleep(60)
 
-        jobs = [gevent.spawn(timer)]
+        gevent.signal(signal.SIGKILL, gevent.kill)
+        gevent.signal(signal.SIGQUIT, gevent.kill)
+        self.jobs = [gevent.spawn(timer)]
         for i in xrange(self.concur):
-            jobs.append(gevent.spawn(worker))
-        gevent.joinall(jobs)
+            self.jobs.append(gevent.spawn(worker))
 
     def add_task(self, task):
-        self.tasks.put_nowait(task)
+        self.tasks.put(task, timeout=60)
--- processors/images.py
+++ processors/images.py
@@ -1,11 +1,16 @@
 # coding=utf-8
 import argparse
 import re
+from hashlib import md5
+import gevent
+import pymongo
+from pymongo.errors import DuplicateKeyError
+from processors import BaseProcessor
 
 __author__ = 'zephyre'
 
 
-class ImageTransfer(object):
+class ImageTransfer(BaseProcessor):
     """
     图像的迁移。从lvxpingpai-img-store迁移到aizou，同时优化imagestore数据存储的格式
     """
@@ -13,16 +18,17 @@ class ImageTransfer(object):
     name = 'image-transfer'
 
     def __init__(self):
-        self.args = None
+        super(ImageTransfer, self).__init__()
+        self.args = self.args_builder()
 
-        self.args_builder()
-
-    def args_builder(self):
+    @staticmethod
+    def args_builder():
         parser = argparse.ArgumentParser()
         parser.add_argument('cmd')
         parser.add_argument('--limit', default=None, type=int)
         parser.add_argument('--skip', default=0, type=int)
-        self.args = parser.parse_args()
+        args, leftover = parser.parse_known_args()
+        return args
 
     @staticmethod
     def bucket_mgr():
@@ -37,49 +43,89 @@ class ImageTransfer(object):
         q = Auth(access_key, secret_key)
         return BucketManager(q)
 
+    @staticmethod
+    def mv_candidates(image):
+        """
+        将图像添加到ImageCandidates里面
+        """
+        from mongo import get_mongodb
+
+        col_cand = get_mongodb('imagestore', 'ImageCandidates', profile='mongo')
+        col_img = get_mongodb('imagestore', 'Images', profile='mongo')
+
+        image_id = image.pop('_id')
+
+        print 'Moving %s' % image['url']
+        image['url_hash'] = md5(image['url']).hexdigest()
+        image['key'] = image['url_hash']
+        col_cand.update({'url_hash': image['url_hash']}, {'$set': image})
+
+        col_img.remove({'_id': image_id})
+
     def run(self):
-        import gevent
         from mongo import get_mongodb
 
         col = get_mongodb('imagestore', 'Images', profile='mongo')
 
-        cursor = col.find({'key': re.compile(r'^assets'), 'bucket': None}, snapshot=True)
+        cursor = col.find({}).sort('_id', pymongo.ASCENDING)
         cursor.skip(self.args.skip)
         if self.args.limit:
             cursor.limit(self.args.limit)
 
         bmgr = self.bucket_mgr()
-        jobs = []
 
-        def gl(entry):
-            bucket_name = entry['bucket'] if 'bucket' in entry else 'lvxingpai-img-store'
-            key = entry['key']
+        # self.total = cursor.count(with_limit_and_skip=True)
+        self.total = 0
 
-            ret, info = bmgr.copy(bucket_name, key, 'aizou', entry['url_hash'])
+        super(ImageTransfer, self).run()
 
-            if ret != {} and info.error == 'file exists':
-                pass
-            else:
-                assert ret == {}
-
-            ret, info = bmgr.stat('aizou', entry['url_hash'])
-            assert ret is not None
-            entry['cTime'] = long(ret['putTime']) / 10000
-            entry['type'] = ret['mimeType']
-            entry['hash'] = ret['hash']
-            entry['size'] = ret['fsize']
-            entry['key'] = entry['url_hash']
-
-            col.save(entry)
-
-        print '%d documents to process...' % cursor.count(with_limit_and_skip=True)
         for entry in cursor:
-            jobs.append(gevent.spawn(gl, entry))
+            def func(val=entry):
+                self.progress += 1
 
-        gevent.joinall(jobs)
+                print 'Processing %s : %s' % (val['url_hash'], val['url'])
+                url_hash = md5(val['url']).hexdigest()
+                key = re.search(r'[0-9a-f]{32}', val['key']).group()
+                if url_hash != val['url_hash'] or key != url_hash:
+                    self.mv_candidates(val)
+                    return
+
+                bucket_name = val['bucket'] if 'bucket' in val else 'lvxingpai-img-store'
+                key = val['key']
+
+                bmgr.copy(bucket_name, key, 'aizou', val['url_hash'])
+
+                ret, info = bmgr.stat('aizou', val['url_hash'])
+                if ret is None and info.status_code == 612:
+                    # 如果爱走里面也找不到，则添加到ImageCandidates库里面，等待后续重新下载
+                    self.mv_candidates(val)
+                    return
+
+                assert ret is not None
+                val['cTime'] = long(ret['putTime']) / 10000
+                val['type'] = ret['mimeType']
+                val['hash'] = ret['hash']
+                val['size'] = ret['fsize']
+                val['key'] = url_hash
+
+                if '_id' in val:
+                    val.pop('_id')
+
+                print 'Updating %s: %s' % (val['url_hash'], val['url'])
+                try:
+                    col.update({'url_hash': val['url_hash']}, {'$set': val}, upsert=True)
+                except DuplicateKeyError:
+                    ret = col.find_one({'key': val['key']})
+                    self.mv_candidates(ret)
+                    col.update({'url_hash': val['url_hash']}, {'$set': val}, upsert=True)
+
+            self.add_task(func)
+            gevent.sleep(0)
+
+        self.join()
 
 
-class ImageValidator(object):
+class ImageValidator(BaseProcessor):
     """
     图像验证
     """
@@ -87,34 +133,21 @@ class ImageValidator(object):
     name = 'image-validate'
 
     def __init__(self):
-        self.args = None
+        super(ImageValidator, self).__init__()
+        self.args = self.args_builder()
 
-        self.args_builder()
-
-    def args_builder(self):
+    @staticmethod
+    def args_builder():
         parser = argparse.ArgumentParser()
         parser.add_argument('cmd')
         parser.add_argument('--limit', default=None, type=int)
         parser.add_argument('--skip', default=0, type=int)
         parser.add_argument('--db', required=True)
         parser.add_argument('--col', required=True)
-        self.args = parser.parse_args()
-
-    @staticmethod
-    def bucket_mgr():
-        from qiniu import Auth
-        from qiniu import BucketManager
-        from mongo import load_config
-
-        conf = load_config()['qiniu']
-
-        access_key = conf['ak']
-        secret_key = conf['sk']
-        q = Auth(access_key, secret_key)
-        return BucketManager(q)
+        args, leftover = parser.parse_known_args()
+        return args
 
     def run(self):
-        import gevent
         from mongo import get_mongodb
 
         col = get_mongodb(self.args.db, self.args.col, profile='mongo')
@@ -126,39 +159,51 @@ class ImageValidator(object):
 
         col_im = get_mongodb('imagestore', 'Images', profile='mongo')
 
-        def func(entry):
-            modified = False
-            for img in entry['images']:
-                key = img['key']
-                match = re.search(r'assets/images/([0-9a-f]{32})', key)
-                if match:
-                    key = match.group(1)
-                ret = col_im.find_one({'key': key})
-                if not ret:
-                    print img
-                assert ret is not None
+        cursor = col.find({}, {'images': 1}).sort('_id', pymongo.ASCENDING)
+        cursor.skip(self.args.skip)
+        if self.args.limit:
+            cursor.limit(self.args.limit)
 
-                if img['key'] != key:
-                    modified = True
-                    img['key'] = key
-
-                if 'url' in img:
-                    modified = True
-                    img.pop('url')
-
-                if 'cropHint' in img:
-                    ch = img['cropHint']
-                    if ch['bottom'] == 0 and ch['right'] == 0:
-                        modified = True
-                        img.pop('cropHint')
-
-            if modified:
-                col.save(entry)
-
-        jobs = []
+        self.total = 0
         for entry in cursor:
-            jobs.append(gevent.spawn(func, entry))
+            def func(val=entry):
+                modified = False
+                if 'images' not in val or not val['images']:
+                    return
 
-        gevent.joinall(jobs)
+                for img in val['images']:
+                    key = img['key']
+                    match = re.search(r'[0-9a-f]{32}', key)
+                    if not match:
+                        continue
+                    new_key = match.group()
+
+                    # 使用new_key去imagestore中查询
+                    ret = col_im.find_one({'$or': [{'key': new_key}, {'url_hash': new_key}]})
+                    if not ret:
+                        print 'Image not exists: %s' % key
+                        continue
+
+                    if img['key'] != key:
+                        modified = True
+                        img['key'] = key
+
+                    if 'url' in img:
+                        modified = True
+                        img.pop('url')
+
+                    if 'cropHint' in img:
+                        ch = img['cropHint']
+                        if ch['bottom'] == 0 and ch['right'] == 0:
+                            modified = True
+                            img.pop('cropHint')
+
+                if modified:
+                    col.update({'_id': val['_id']}, {'$set': {'images': val['images']}})
+
+            self.add_task(func)
+            gevent.sleep(0)
+
+        super(ImageValidator, self).run()
 
 
